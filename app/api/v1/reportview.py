4685a2c16157459fd133dcf3202a73c8b7549d54
Benedict <Jose@gmail.com>
2018-11-29 19:25:41 +0300

    [finishes #162216433] Create post end point

---
+++ app/api/v1/reportview.py
@@ -0,0 +1,22 @@
+import json
+from flask_restful import Resource
+from flask import jsonify,make_response,request
+from reportmodel import incidence, myRedflags
+
+
+
+class MyReports(Resource):
+
+    def __init__(self):
+        self.db = incidence
+        self.items = myRedflags()
+    def post (self):
+
+        data = request.get_json()
+
+        print(data["name"])
+
+        response = self.items.save(data["name"], data["flag"], data["location"])
+        return make_response(jsonify({
+            "My Reports" : response
+        }),201)
