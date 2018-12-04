543ccff90c10fcc165acf5233bc00a9cd1afc973
Flockaman <42209221+flocka12@users.noreply.github.com>
2018-12-05 02:26:59 +0300

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,17 +1,1 @@
-# class Config (object):
-#     DEBUB = True
 
-# class DevelopmentConfig (Config):
-#     SQALCHEMY_ECHO = True
-
-# class ProductionConfig (Config):
-#     DEBUG = False
-
-# class TestingConfig (Config):
-#     TESTING = True
-
-# app_config = {
-#     'development': DevelopmentConfig,
-#     'production': ProductionConfig,
-#     'testing': TestingConfig
-# }
